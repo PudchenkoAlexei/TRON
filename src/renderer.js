08b821d2f41a3253173e58267c50d09475590d58
PudchenkoAlexei <leshapudchenko@gmail.com>
2025-12-09 02:04:07 +0200

    new style

--- src/renderer.js
+++ src/renderer.js
@@ -1,5 +1,66 @@
 import { WORLD_SIZE, GRID_STEP } from './config.js';
 
+function drawLightcycle(ctx, b, camX, camY) {
+    const sx = b.x - camX;
+    const sy = b.y - camY;
+
+    ctx.save();
+    ctx.translate(sx, sy);
+    ctx.rotate(b.angle);
+
+    const WHEEL_R = 12;
+    const CORE_R = WHEEL_R * 0.55;
+    const BODY_W = 8;
+    const BODY_L = 52;
+
+    ctx.shadowColor = b.color;
+    ctx.shadowBlur = 18;
+    ctx.fillStyle = b.color;
+
+    const frontX = BODY_L * 0.5;
+    const backX = -BODY_L * 0.5;
+
+    ctx.beginPath();
+    ctx.arc(frontX, 0, WHEEL_R, 0, Math.PI * 2);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(backX, 0, WHEEL_R, 0, Math.PI * 2);
+    ctx.fill();
+
+    ctx.shadowBlur = 0;
+    ctx.fillStyle = "#000000cc";
+
+    ctx.beginPath();
+    ctx.arc(frontX, 0, CORE_R, 0, Math.PI * 2);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(backX, 0, CORE_R, 0, Math.PI * 2);
+    ctx.fill();
+
+    ctx.shadowBlur = 12;
+    ctx.fillStyle = b.color;
+    ctx.beginPath();
+    ctx.roundRect(backX, -BODY_W / 2, BODY_L, BODY_W, 4);
+    ctx.fill();
+
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = "#ffffffbb";
+    ctx.beginPath();
+    ctx.moveTo(-10, -BODY_W * 0.45);
+    ctx.lineTo(10, -BODY_W * 0.45);
+    ctx.stroke();
+
+    ctx.shadowBlur = 5;
+    ctx.fillStyle = "#ffffffdd";
+    ctx.beginPath();
+    ctx.ellipse(0, 0, 7, 3.6, 0, 0, Math.PI * 2);
+    ctx.fill();
+
+    ctx.restore();
+}
+
 export function renderWorld(world, ctx, canvas) {
     const w = canvas.width;
     const h = canvas.height;
@@ -14,8 +75,13 @@ export function renderWorld(world, ctx, canvas) {
     ctx.save();
     ctx.translate(w / 2, h / 2);
 
-    ctx.strokeStyle = '#101010';
+    ctx.fillStyle = '#020205';
+    ctx.fillRect(-w, -h, w * 2, h * 2);
+
+    ctx.strokeStyle = '#0d1a2a';
     ctx.lineWidth = 1;
+    ctx.shadowColor = '#0a537d';
+    ctx.shadowBlur = 3;
 
     for (let gx = -WORLD_SIZE / 2; gx <= WORLD_SIZE / 2; gx += GRID_STEP) {
         ctx.beginPath();
@@ -31,69 +97,47 @@ export function renderWorld(world, ctx, canvas) {
         ctx.stroke();
     }
 
+    ctx.shadowBlur = 0;
+
     for (const b of world.bikes) {
         ctx.strokeStyle = b.color;
-        ctx.lineWidth = 3;
+        ctx.lineWidth = 6;
 
-        if (b.trail.length > 0) {
-            ctx.save();
-            ctx.shadowColor = b.color;
-            ctx.shadowBlur = 10;
+        ctx.save();
+        ctx.shadowColor = b.color;
+        ctx.shadowBlur = 22;
 
+        const tr = b.trail;
+        if (tr.length > 0) {
             ctx.beginPath();
-            for (let i = 0; i < b.trail.length; i++) {
-                const p = b.trail[i];
-                const sx = p.x - camX;
-                const sy = p.y - camY;
-                if (i === 0) ctx.moveTo(sx, sy);
-                else ctx.lineTo(sx, sy);
+            ctx.moveTo(tr[0].x - camX, tr[0].y - camY);
+            for (let i = 1; i < tr.length; i++) {
+                ctx.lineTo(tr[i].x - camX, tr[i].y - camY);
             }
             ctx.stroke();
-
-            ctx.restore();
         }
+
+        ctx.restore();
     }
 
     for (const bonus of world.bonuses) {
         if (!bonus.alive) continue;
         const sx = bonus.x - camX;
         const sy = bonus.y - camY;
+
+        ctx.save();
+        ctx.shadowColor = '#ffff66';
+        ctx.shadowBlur = 15;
+        ctx.fillStyle = '#fff67f';
         ctx.beginPath();
-        ctx.arc(sx, sy, 8, 0, Math.PI * 2);
-        ctx.fillStyle = '#ffff00';
+        ctx.arc(sx, sy, 10, 0, Math.PI * 2);
         ctx.fill();
+        ctx.restore();
     }
 
     for (const b of world.bikes) {
         if (!b.alive) continue;
-
-        const sx = b.x - camX;
-        const sy = b.y - camY;
-
-        ctx.save();
-        ctx.translate(sx, sy);
-        ctx.rotate(b.angle);
-
-        ctx.shadowColor = b.color;
-        ctx.shadowBlur = 15;
-
-        ctx.fillStyle = b.color;
-
-        ctx.beginPath();
-        ctx.roundRect(-16, -7, 32, 14, 7);
-        ctx.fill();
-
-        ctx.beginPath();
-        ctx.arc(14, 0, 4, 0, Math.PI * 2);
-        ctx.fill();
-
-        ctx.shadowBlur = 4;
-        ctx.fillStyle = "#ffffffaa";
-        ctx.beginPath();
-        ctx.ellipse(-6, 0, 4, 2.2, 0, 0, Math.PI * 2);
-        ctx.fill();
-
-        ctx.restore();
+        drawLightcycle(ctx, b, camX, camY);
     }
 
     ctx.restore();
